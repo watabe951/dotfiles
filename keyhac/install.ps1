2e91f80bfc2b3a93f9819792f016b10b7bdb6789
YuuyaWatabe <watabe951@gmail.com>
2021-12-10 17:11:48 +0900

    インストール時に作業ファイルを汚さないようにしてみた

--- keyhac/install.ps1
+++ keyhac/install.ps1
@@ -1,1 +1,10 @@
+curl https://crftwr.github.io/keyhac/download/keyhac_182.zip -o ./keyhac_182.zip
+unzip ./keyhac_182.zip
+mv ./keyhac/ ~/keyhac
+mkdir -p $(dirname $env:APPDATA/Keyhac/config.py)
 gsudo ln -T -s -f $HOME/dotfiles/keyhac/config.py $env:APPDATA/Keyhac/config.py
+
+$starup = $(Get-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders').startup
+gsudo ln -T -s  -f $HOME/keyhac/keyhac.exe $($starup + "\keyhac.exe")
+rm ./keyhac_182.zip
+rmdir .\keyhac\
