a285f73427dbd7dc1e11dc5d6377f4f8ed7bfd53
Yuuya Watabe <watabe951@gmail.com>
2021-12-12 02:35:27 +0900

    パスをなおした

--- nvim/install.ps1
+++ nvim/install.ps1
@@ -5,4 +5,4 @@ if (-Not(Test-Path $HOME\.config\nvim)){
 if (Test-Path $HOME\.config\nvim\init.vim){
 	Rename-Item -Path $HOME\.config\nvim\init.vim -NewName $HOME\.config\nvim\init.vim.backup
 }
-gsudo New-Item -Path $HOME/.config/nvim -Name init.vim -Value $HOME/.config/nvim/init.vim -ItemType SymbolicLink -Force
+gsudo New-Item -Path $HOME/.config/nvim -Name init.vim -Value $HOME/dotfiles/nvim/init.vim -ItemType SymbolicLink -Force
